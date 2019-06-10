4da76eb15f1fda832cbf3cafc17ed0d2d09d05e5
brfreitag <brufreitas@gmail.com>
2019-06-10 00:33:49 -0300

    Using ftok

--- fila_out.php
+++ fila_out.php
@@ -1,14 +1,16 @@
-<?php 
-if ( sizeof($argv)<2 ) { 
-        echo "Usage: $argv[0] stat|send|receive|remove msgType MSG [msg] \n\n" ; 
-        echo "   EX: $argv[0] send 1 \"This is no 1\" \n" ; 
-        echo "       $argv[0] receive ID \n" ; 
-        echo "       $argv[0] stat \n" ; 
-        echo "       $argv[0] remove \n" ; 
-        exit; 
-} 
+<?php
+if (sizeof($argv) < 2) {
+  print "Usage: $argv[0] stat|send|receive|remove msgType MSG [msg] \n\n" ; 
+  print "   EX: $argv[0] send 1 \"This is no 1\" \n" ; 
+  print "       $argv[0] receive ID \n" ; 
+  print "       $argv[0] stat \n" ; 
+  print "       $argv[0] remove \n" ; 
+  exit; 
+}
 
-$MSGKey = "123456" ; 
+if (($MSGKey = ftok("/var/www/html/fila/fila_out.php", "S")) == -1) {
+  die("ftok");
+}
 
 ## Create or attach to a message queue 
 $seg = msg_get_queue($MSGKey) ; 
@@ -16,20 +18,20 @@ $seg = msg_get_queue($MSGKey) ;
 switch ( $argv[1] ) { 
     case "send": 
         msg_send($seg, $argv[2], $argv[3]); 
-        echo "msg_send done...\n" ; 
+        print "msg_send done...\n" ; 
         break; 
         
     case "receive": 
         $stat = msg_stat_queue( $seg ); 
-        echo 'Messages in the queue: '.$stat['msg_qnum']."\n"; 
+        print 'Messages in the queue: '.$stat['msg_qnum']."\n"; 
         if ( $stat['msg_qnum']>0 ) { 
             msg_receive($seg, $argv[2], $msgtype, 1024, $data); 
             var_dump($msgtype); 
             var_dump($data); 
-            echo "\n"; 
+            print "\n"; 
         } 
         else { 
-            echo "No Msg...\n"; 
+            print "No Msg...\n"; 
         } 
         break; 
     
